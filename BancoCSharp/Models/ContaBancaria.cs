24e83e074a7cc68f069a5d16e958741ba8e686f7
riicardods <riicardoljn@icloud.com>
2023-10-19 20:30:10 -0300

    Adicionar arquivos de projeto.

---
+++ BancoCSharp/Models/ContaBancaria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoCSharp.Models
+{
+    public class ContaBancaria
+    {
+        public Titular Titular {  get; set; }
+        public double Saldo { get; private set; }
+        public DateTime DataAbertura { get; private set; }
+
+
+
+        public ContaBancaria(Titular titular, double saldoAbertura)
+        {
+            Titular = titular;
+            Saldo = saldoAbertura;
+            DataAbertura = DateTime.Now;
+        }
+
+        public ContaBancaria()
+        {
+        }
+    }
+}
---
+++ BancoCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using BancoCSharp.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("*******************************************************");
+        Console.WriteLine("****************** Banco CSharp ***********************");
+        Console.WriteLine("*******************************************************");
+        Console.WriteLine();
+
+
+
+        var endereco = new Endereco
+        {
+            Cep = "28000485",
+            Rua = "Rua xyz",
+            Numero = 52
+        };
+        var titular01 = new Titular("jose da silva", "12345678912345", "219877542150", endereco);
+
+
+        var conta01 = new ContaBancaria(titular01, 100);
+
+
+
+        Console.WriteLine(conta01.Saldo);
+    }
+}
